894e4d8040befb0bdb6a35f6cff23a1bca7e36f2
tangzx <272669294@qq.com>
2018-08-04 17:33:20 +0800

    en & zh_CN

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
     VERSION: '1.0',
-    LANGUAGE: 'zh_CN',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: false,
